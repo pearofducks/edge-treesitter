eb9d1dcbee768d171c207a6877cd2c76bdb4105d
Dave Honneffer <pearofducks@gmail.com>
2026-04-22 17:49:21 +0200

    Handle indents better

--- queries/edge/indents.scm
+++ queries/edge/indents.scm
@@ -1,3 +1,29 @@
-(directive_start) @indent.begin
-(directive_end) @indent.end @indent.dedent
-(conditional_keyword) @indent.branch @indent.dedent
+; inherits: html
+
+[
+  (conditional)
+  (loop)
+  (component)
+  (slot)
+  (stack_push)
+] @indent.begin
+
+(conditional
+  (directive_end) @indent.end)
+
+(loop
+  (directive_end) @indent.end)
+
+(component
+  (directive_end) @indent.end)
+
+(slot
+  (directive_end) @indent.end)
+
+(stack_push
+  (directive_end) @indent.end)
+
+[
+  (directive_end)
+  (conditional_keyword)
+] @indent.branch
